caf73c7e3928747ee76fe9f26aef29737a37e818
Jochen Platzgummer <jochen.platzgummer@bi-s.it>
2014-01-22 08:33:31 +0100

    motd.sh: SSH MOTD (Message Of The Day) issue: Logon Screen Message

---
+++ os customizing/etc/profile.d/motd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+echo -e "
+##################################
+#
+# Osiris 2.0 NMS
+#
+# All rights reserved
+# BI-S des Jochen Platzgummer
+# info@bi-s.it
+# 
+# Welcome to `hostname`
+#
+# This system is running `cat /etc/redhat-release`
+# kernel is `uname -r`
+#
+# You are logged in as `whoami`
+#
+##################################
+"
