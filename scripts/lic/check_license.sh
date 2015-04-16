c9d70bae9a8a17a5bd9618cf782323a585210670
Jochen Platzgummer <jochen.platzgummer@unifix.it>
2015-04-16 23:17:24 +0200

    license file encrypted license file & check with expire date

--- scripts/lic/check_license.sh
+++ scripts/lic/check_license.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-SYSGUID="$(dmidecode |grep UUID)"
-COMP_NAME_LIC="$(cat /etc/osiris.lic |grep 'Company:')"
-COMP_NAME_LIC=${COMP_NAME_LIC:9}
-SN_LIC="$(cat /etc/osiris.lic |grep 'Serial Number:')"
-SN_LIC=${SN_LIC:15}
+#decode license file
+openssl enc -d -aes-256-cbc -pass file:/opt/bi-s/software/scripts/gpg/.gpg_passwd.txt -in /etc/osiris.lic -out /tmp/osiris.lic.txt
 
-S1="$SYSGUID $COMP_NAME_LIC"
+#read system uuid
+SYSGUID="$(dmidecode |grep UUID)"
+
+#read license file
+COMP_NAME_LIC="$(cat /tmp/osiris.lic.txt |grep 'Company:')"
+COMP_NAME_LIC=${COMP_NAME_LIC:9}
+SN_LIC="$(cat /tmp/osiris.lic.txt |grep 'Serial number:')"
+SN_LIC=${SN_LIC:15}
+EXP_DATE="$(cat /tmp/osiris.lic.txt |grep 'Expire date:')"
+EXP_DATE=${EXP_DATE:13}
+
+#delete txt license file
+rm -rf /tmp/osiris.lic.txt
+
+S1="$SYSGUID $COMP_NAME_LIC $EXP_DATE"
 SN="$(echo -n "$S1" | md5sum)"
 
 echo "$(cat /opt/bi-s/software/scripts/lic/issue)" > /etc/issue
@@ -15,17 +26,50 @@ rm -rf /opt/bi-s/software/scripts/lic/shutdown
 touch /opt/bi-s/software/scripts/lic/shutdown
 chmod +x /opt/bi-s/software/scripts/lic/shutdown
 
-if [ "$SN" = "$SN_LIC" ]; then
+#Date Diff rechnen
+if [ "$EXP_DATE" != "never" ]; then
+  EXP_DATE2=$(date -d $EXP_DATE +%s)
+  TODAY=$(date +%s)
+  DIFF_DAYS=$(( ($EXP_DATE2-$TODAY)/(60*60*24) ))
+else
+  DIFF_DAYS=999
+fi
+
+if [ "$SN" = "$SN_LIC" ] && (( "$DIFF_DAYS" >= 1 )); then
   echo "license is valid"
   echo "The system has a valid license key" >> /etc/issue
-else
-  echo "license is not valid"
-  echo "The system has a NON valid license key" >> /etc/issue
+    if (( "$DIFF_DAYS" >= 1 )) && (( "$DIFF_DAYS" < 30 )); then
+      echo "Your license will be expire in $DIFF_DAYS days"
+      echo "Your license will be expire in $DIFF_DAYS days" >> /etc/issue
+      SUBJECT="$COMP_NAME_LIC: Osiris licensed will expire in $DIFF_DAYS days"
+      echo "$SYSGUID" >> email.txt
+      echo "HOSTNAME: `hostname`" >> email.txt
+      echo "IFCONFIG:" >> email.txt
+      echo "`ifconfig`" >> email.txt
+      TEXT="$(cat email.txt)"
+      MAIL="Subject: $SUBJECT\nFrom: osiris@bi-s.it\nTo: jochen.platzgummer@bi-s.it\n\n$TEXT"
+      rm -rf email.txt
+      echo -e $MAIL | sendmail -t
+      exit $?
+    fi
+elif [ "$SN" != "$SN_LIC" ] || (( "$DIFF_DAYS" <= 0 )); then
+  if  (( "$DIFF_DAYS" <= 0 )); then
+    echo "your license key is expired"
+    echo "The license key is EXPIRED" >> /etc/issue
 
-  echo "shutdown -P now" >> /opt/bi-s/software/scripts/lic/shutdown
+    echo "shutdown -P now" >> /opt/bi-s/software/scripts/lic/shutdown
 
-  SUBJECT="Osiris not licensed"
-  echo "Osiris ist nicht lizenziert, Shutdown in 4 Stunden" > email.txt
+    SUBJECT="$COMP_NAME_LIC: Osiris licensed is expired"
+    echo "Osiris lizenziert ist verfallen, Shutdown in 4 Stunden" > email.txt
+  else
+    echo "license is not valid"
+    echo "The system has a NON valid license key" >> /etc/issue
+
+    echo "shutdown -P now" >> /opt/bi-s/software/scripts/lic/shutdown
+
+    SUBJECT="$COMP_NAME_LIC: Osiris not licensed"
+    echo "Osiris ist nicht lizenziert, Shutdown in 4 Stunden" > email.txt
+  fi
   echo "$SYSGUID" >> email.txt
   echo "HOSTNAME: `hostname`" >> email.txt
   echo "IFCONFIG:" >> email.txt
--- scripts/lic/create_license.sh
+++ scripts/lic/create_license.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 echo "Bitte den Company Namen eingeben:"
 read COMP_NAME
+echo "Bitte das Gültigkeitsdatum eingeben (YYYYMMDD oder never):"
+read EXPIRE_DATE
 
 GUID="$(dmidecode |grep UUID)"
 
-S1="$GUID $COMP_NAME"
+S1="$GUID $COMP_NAME $EXPIRE_DATE"
 SN="$(echo -n "$S1" | md5sum)"
 
 echo "Die Seriennummer für $COMP_NAME lautet:"
 echo "Company: $COMP_NAME"
-echo "Serial Number: $SN"
+echo "Expire date: $EXPIRE_DATE"
+echo "Serial number: $SN"
 
-echo "Osiris License File" > osiris.lic
-echo "Company: $COMP_NAME" >> osiris.lic
-echo "Serial Number: $SN" >> osiris.lic
+echo "Osiris License File" > osiris.lic.txt
+echo "Company: $COMP_NAME" >> osiris.lic.txt
+echo "Serial number: $SN" >> osiris.lic.txt
+echo "Expire date: $EXPIRE_DATE" >> osiris.lic.txt
+
+openssl enc -aes-256-cbc -pass file:/opt/bi-s/software/scripts/gpg/.gpg_passwd.txt -in osiris.lic.txt -out osiris.lic
