4fcc5fa10c74c0e10c8086a141f9b0f4d46df277
Jochen Platzgummer <jochen.platzgummer@unifix.it>
2015-08-20 11:57:24 +0200

    sync_poller  - die konfigurierten Poller werden über die Centreon CLAPI ausgelesen  - alle aktiven Poller werden syncroniesiert, sofern erreichbar

--- scripts/sync/sync_poller.sh
+++ scripts/sync/sync_poller.sh
@@ -1,8 +1,27 @@
-#!/bin/bash
+CENT_USER="admin"
+CENT_PWD="password"
 
-if ping -c 1 $1 &> /dev/null
-then
-  rsync -azrP --delete --progress /usr/lib/nagios/plugins/ root@$1:/usr/lib/nagios/plugins/ >> /var/log/sync-poller.log 2>&1
-else
- echo "Poller $1 not reachable" >> /var/log/sync-poller.log 2>&1
-fi
+function find_poller() {
+  /usr/share/centreon/www/modules/centreon-clapi/core/centreon -u $CENT_USER -p $CENT_PWD -o INSTANCE -a show > /tmp/poller.txt
+  IFS=";"
+  while read ID NAME LOCALHOST IP ACTIVATE STATUS OTHERS
+  do
+    #echo $ID $NAME $LOCALHOST $IP $ACTIVATE $STATUS
+    if [ "$LOCALHOST" = "0" ] && [ "$ACTIVATE" = "1"  ]; then
+      sync_poller
+    fi
+  done < /tmp/poller.txt
+  rm -rf /tmp/poller.txt
+
+}
+
+function sync_poller() {
+  if ping -c 1 $IP &> /dev/null
+  then
+    rsync -azrP --delete --progress /usr/lib/nagios/plugins/ root@$IP:/usr/lib/nagios/plugins/ >> /var/log/sync-poller.log 2>&1
+  else
+    echo "Poller $IP not reachable" >> /var/log/sync-poller.log 2>&1
+  fi
+}
+
+find_poller
