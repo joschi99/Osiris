7bad4313f4460bb327166434db24bf0ddb9d75b1
joschi99 <jochen.platzgummer@bi-s.it>
2016-09-09 10:59:25 +0200

    VIP centreon-vmware config

---
+++ os customizing/etc/centreon/centreon_vmware.pm
@@ -0,0 +1,13 @@
+%centreon_vmware_config = (
+        vsphere_server => {
+                        'GEOS' => {'url' => 'https://xxx.xxx.xxx.xxx/sdk',
+                                     'username' => 'USER',
+                                     'password' => 'PWD'},
+                        'TEXEL' => {'url' => 'https://xxx.xxx.xxx.xxx/sdk',
+                                     'username' => 'USER',
+                                     'password' => 'PWD'},
+                        'VIP' => {'url' => 'https://xxx.xxx.xxx.xxx/sdk',
+                                     'username' => 'USER',
+                                     'password' => 'PWD'},
+          }
+);
