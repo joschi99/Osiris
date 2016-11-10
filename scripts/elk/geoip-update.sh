97587418d22c15277320da82ba69203b1688931b
joschi99 <jochen.platzgummer@bi-s.it>
2016-11-10 15:39:29 +0100

    upgrade to ELK 5.0 (see #62)
    - update elastic templates
    - update logstash config
    - update geoip-update to GeoLite2
    - update curator 4.2.1 cronjob

--- scripts/elk/geoip-update.sh
+++ scripts/elk/geoip-update.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 ###############################################################################
 #
-# geoip-update.sh - Download and update GEOIP Database
+# geoip-update.sh - Download and update GeoLite2 Database
 #
 # Copyright (c) 2016 i-Vertix NMS (info@pgum.eu)
 #
 # Development:
 #  Jochen Platzgummer
 #
-# Version 1.0
+# Version 1.1
 #
 # Changelog
-#   30.09.2015: Initial release
+#       10.11.2016: ELK 5.x supports now GeoLite2 databases
+#       30.09.2015: Initial release
 ###############################################################################
 
-cd /opt/bi-s/software/ELK/geolite
-wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
-gzip -d GeoLiteCity.dat.gz
-mv -f GeoLiteCity.dat /etc/logstash
+cd /opt/bi-s/software/ELK
+wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz
+gzip -d GeoLite2-City.mmdb.gz
+mv -f GeoLite2-City.mmdb /etc/logstash
+
+# restart Logstash
+initctl restart logstash
